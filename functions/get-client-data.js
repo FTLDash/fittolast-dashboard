a653fd2560aa4e7e60869058fb4989586ec8a09b
FTLDash <james@fittolast.co.uk>
2026-04-22 07:24:22 +0100

    Update get-client-data.js

--- functions/get-client-data.js
+++ functions/get-client-data.js
@@ -1,29 +1,44 @@
-import * as crypto from 'crypto';
-
 const SHEET_ID = '12LcaLcrGPTebS_Ikr5wIK_v7h0-iVDOUEdwT7jy59qU';
 
 function base64url(str) {
-  return Buffer.from(str).toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+  return btoa(str).replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
 }
 
 async function getAccessToken(credentials) {
-  const now = Math.floor(Date.now() / 1000);
+  const now    = Math.floor(Date.now() / 1000);
   const header = base64url(JSON.stringify({ alg: 'RS256', typ: 'JWT' }));
   const claim  = base64url(JSON.stringify({
-    iss: credentials.client_email,
+    iss:   credentials.client_email,
     scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
-    aud: 'https://oauth2.googleapis.com/token',
-    exp: now + 3600,
-    iat: now
+    aud:   'https://oauth2.googleapis.com/token',
+    exp:   now + 3600,
+    iat:   now
   }));
-  const sigInput = `${header}.${claim}`;
-  const privateKey = crypto.createPrivateKey(credentials.private_key);
-  const signature  = crypto.sign('sha256', Buffer.from(sigInput), privateKey);
-  const jwt = `${sigInput}.${signature.toString('base64').replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_')}`;
+
+  const sigInput  = `${header}.${claim}`;
+  const keyData   = credentials.private_key
+    .replace(/-----BEGIN PRIVATE KEY-----/, '')
+    .replace(/-----END PRIVATE KEY-----/, '')
+    .replace(/\n/g, '');
+  const binaryKey = Uint8Array.from(atob(keyData), c => c.charCodeAt(0));
+
+  const cryptoKey = await crypto.subtle.importKey(
+    'pkcs8', binaryKey,
+    { name: 'RSASSA-PKCS1-v1_5', hash: 'SHA-256' },
+    false, ['sign']
+  );
+
+  const encoder   = new TextEncoder();
+  const sigBuffer = await crypto.subtle.sign('RSASSA-PKCS1-v1_5', cryptoKey, encoder.encode(sigInput));
+  const sigBase64 = btoa(String.fromCharCode(...new Uint8Array(sigBuffer)))
+    .replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+
+  const jwt = `${sigInput}.${sigBase64}`;
+
   const res  = await fetch('https://oauth2.googleapis.com/token', {
-    method: 'POST',
+    method:  'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: `grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=${jwt}`
+    body:    `grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=${jwt}`
   });
   const data = await res.json();
   if (!data.access_token) throw new Error('Token error: ' + JSON.stringify(data));
@@ -31,7 +46,7 @@ async function getAccessToken(credentials) {
 }
 
 async function getSheetData(token, tab) {
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${encodeURIComponent(tab + '!A1:ZZ')}`;
+  const url  = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${encodeURIComponent(tab + '!A1:ZZ')}`;
   const res  = await fetch(url, { headers: { Authorization: `Bearer ${token}` } });
   const data = await res.json();
   return data.values || [];
@@ -50,7 +65,7 @@ export async function onRequest(context) {
 
   try {
     const credentials = JSON.parse(context.env.GOOGLE_SERVICE_ACCOUNT_JSON);
-    const token = await getAccessToken(credentials);
+    const token       = await getAccessToken(credentials);
 
     const scanRows     = await getSheetData(token, 'Scan Data');
     const scanHeaders  = scanRows[0] || [];
